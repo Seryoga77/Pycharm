a2077a3b8aec424662b3761b54a8a9335f5996ff
Seryoga77 <79380290+Seryoga77@users.noreply.github.com>
2024-07-08 13:02:40 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+result_list = []
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    if my_list[index] == 0:
+        index += 1
+        continue
+    result_list.append(my_list[index])
+    index += 1
+print(result_list)
+
