01c2d909a097e35fbd6d476e77f1f93d3394123f
Seryoga77 <79380290+Seryoga77@users.noreply.github.com>
2025-01-06 20:47:33 +0300

    Add files via upload

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .user import User
-from .task import Task
-
-
-from sqlalchemy.schema import CreateTable
-print(CreateTable(Task.__table__))
-
-from sqlalchemy.schema import CreateTable
-print(CreateTable(User.__table__))
