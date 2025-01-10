87e5ec8f8352372cd9e57a3366ce39cc57ff4d3b
Seryoga77 <79380290+Seryoga77@users.noreply.github.com>
2025-01-10 13:56:55 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,29 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated
 
 app = FastAPI()
 
 users = {'1': 'Имя: Example, возраст: 18'}
 
 
-@app.get('/users')
-async def get_user_page() -> dict:
+@app.get("/users")
+async def get_users():
     return users
 
 
-@app.post('/user/{username}/{age}')
-async def register_user(username: str, age: int) -> str:
-    user_id = str(int(max(users, key=int)) + 1)
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'User {user_id} is registered'
+@app.post("/user/{username}/{age}")
+async def add_user(
+        username: Annotated[str, Path(min_length=1, max_length=50, description="Имя пользователя")],
+        age: Annotated[int, Path(ge=1, le=120, description="Возраст пользователя")]
+):
+    new_id = str(max(map(int, users.keys())) + 1)
+    users[new_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {new_id} is registered"
 
 
-@app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> str:
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'The user {user_id} has been updated'
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(
+        user_id: Annotated[str, Path(description="ID пользователя")],
+        username: Annotated[str, Path(min_length=1, max_length=50, description="Имя пользователя")],
+        age: Annotated[int, Path(ge=1, le=120, description="Возраст пользователя")]
+):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} is updated"
 
 
-@app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
-    users.pop(user_id)
-    return f'User {user_id} has been deleted'
+@app.delete("/user/{user_id}")
+async def delete_user(
+        user_id: Annotated[str, Path(description="ID пользователя")]
+):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
+    del users[user_id]
+    return f"User {user_id} has been deleted"
