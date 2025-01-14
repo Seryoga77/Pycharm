dbb4734091fdd3a0d8c8755f88217694aad19ad4
Seryoga77 <79380290+Seryoga77@users.noreply.github.com>
2025-01-14 12:57:46 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -14,10 +14,10 @@ async def read_admin():
 
 
 @app.get("/user/{user_id}")
-async def read_user(user_id: int):
+async def read_user(user_id: str):
     return f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get("/user")
-async def read_user_info(username: str, age: int):
+async def read_user_info(username: str, age: str):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
