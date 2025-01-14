00a94fbd2184d0133f53d7413c680e3bdd33b962
Seryoga77 <79380290+Seryoga77@users.noreply.github.com>
2025-01-14 12:52:52 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
-from fastapi import Query
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
-    return {"message": "Главная страница"}
+async def read_root():
+    return "Главная страница"
+
 
 @app.get("/user/admin")
-def read_admin():
-    return {"message": "Вы вошли как администратор"}
+async def read_admin():
+    return "Вы вошли как администратор"
+
 
 @app.get("/user/{user_id}")
-def get_user_number(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def read_user(user_id: int):
+    return f"Вы вошли как пользователь № {user_id}"
+
 
 @app.get("/user")
-def get_user_info(username: str = Query(...), age: int = Query(...)):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def read_user_info(username: str, age: int):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
