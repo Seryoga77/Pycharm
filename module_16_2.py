79cb56cc13908617cf2bb82818bd6ad076edb679
Seryoga77 <79380290+Seryoga77@users.noreply.github.com>
2025-01-23 14:15:19 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -1,45 +1,30 @@
-from fastapi import FastAPI, Path, Query
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return JSONResponse(content={"message": "Главная страница"})
+    return {"message": "Главная страница"}
+
 
 @app.get("/user/admin")
 def read_admin():
-    return JSONResponse(content={"message": "Вы вошли как администратор"})
+    return {"message": "Вы вошли как администратор"}
+
 
 @app.get("/user/{user_id}")
 def read_user(
-    user_id: int = Path(
-        ...,
-        ge=1,
-        le=100,
-        description="Введите ID пользователя",
-        example=1
-    )
+    user_id: Annotated[int, Path(description="Enter User ID", ge=1, le=100, examples={"example": 1})]
 ):
-    return JSONResponse(content={"message": f"Вы вошли как пользователь № {user_id}"})
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
 
 @app.get("/user/{username}/{age}")
 def read_user_info(
-    username: str = Path(
-        ...,
-        min_length=5,
-        max_length=20,
-        description="Введите имя пользователя",
-        example="UrbanUser"
-    ),
-    age: int = Path(
-        ...,
-        ge=18,
-        le=120,
-        description="Введите возраст",
-        example=24
-    )
+    username: Annotated[str, Path(description="Enter username", min_length=5, max_length=20, examples={"example": "UrbanUser"})],
+    age: Annotated[int, Path(description="Enter age", ge=18, le=120, examples={"example": 24})]
 ):
-    return JSONResponse(content={"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"})
-
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
